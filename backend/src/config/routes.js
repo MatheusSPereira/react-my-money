b956e535af5d9190c899c97346ef768f51ba18d4
MatheusSPereira <githubmatheus@outlook.com>
2020-01-16 22:43:48 -0300

    add auth classes and methods

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,11 +1,26 @@
 const express = require('express')
+const auth = require('./auth')
 
 module.exports = function (server) {
-    // API Routes
-    const router = express.Router()
-    server.use('/api', router)
+    /*
+    * Rotas protegidas por Token JWT
+    */
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
+    
+    protectedApi.use(auth)
 
-    // Billing Cycles Routes
-    const billingCycle = require('../api/billingCycle/billingCycleService')
-    billingCycle.register(router, '/billingCycles')
+    const BillingCycle = require('../api/billingCycle/billingCycleService')
+    BillingCycle.register(protectedApi, '/billingCycles')
+    
+    /*
+    * Rotas abertas
+    */
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+
+    const AuthService = require('../api/user/AuthService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
 }
